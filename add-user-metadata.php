fa15c8d39c07b13f505d33e4bea542c836d5fccc
RayQ <ray@qstudio.us>
2020-08-28 15:32:06 +0100

    0.5.8 removed distignore

--- add-user-metadata.php
+++ add-user-metadata.php
@@ -4,7 +4,7 @@
 Plugin Name: Import User Meta Data from CSV
 Plugin URI: http://qstudio.us/plugins/
 Description: Bulk add user Metadata from a text list ( csv ) - checking for existing usermeta data to avoid duplicate entries.
-Version: 0.5.7
+Version: 0.5.8
 Author: Q Studio
 Author URI: http://qstudio.us/
 License: GPL2
